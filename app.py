1092fbe6e2db0d767a5cdca39ff5c525be7ecac3
NANMA K V <nanma.ds24@duk.ac.in>
2025-11-03 10:18:20 +0530

    Update GNews API key in app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if st.button("Analyze Sentiment"):
     st.write(f"Fetching news for: **{topic}** ...")
 
     # You can use GNews API or NewsAPI — here’s an example using GNews
-    url = f"https://gnews.io/api/v4/search?q={topic}&lang=en&country=in&max=5&apikey=YOUR_API_KEY"
+    url = f"https://gnews.io/api/v4/search?q={topic}&lang=en&country=in&max=5&apikey=be0238a91b7f6382dc296c4c273af92b"
     response = requests.get(url)
     data = response.json()
 
